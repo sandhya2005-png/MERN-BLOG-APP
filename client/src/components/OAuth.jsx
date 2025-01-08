c037b0822514929e27e6e03c2b004fe5b4f43b33
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2025-01-08 16:41:00 +0530

    make the dashboard private

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -34,7 +34,7 @@ export default function OAuth() {
             }
         }
   return (
-    <Button type="button" className="bg-gradient-to-r from-pink-500 to-purple-500 text-white font-medium px-4 py-1 text-sm rounded-md flex items-center whitespace-nowrap" onClick={handleGoogleClick}>
+    <Button type="button" className="bg-gradient-to-r from-pink-500 to-purple-400 text-white font-medium px-3 py-0.5 text-sm rounded-md flex items-center whitespace-nowrap" onClick={handleGoogleClick}>
        <AiFillGoogleCircle className="w-6 h-6 mr-2" />
        Continue with Google
     </Button>
