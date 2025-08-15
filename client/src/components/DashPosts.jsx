2fecabe577421a9b3657f2c4671d28f0385ba152
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2025-08-15 15:20:28 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,4 +1,4 @@
-import { Table, TableCell, TableHeadCell } from 'flowbite-react'
+import { Button, Table, TableCell, TableHeadCell } from 'flowbite-react'
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
@@ -6,15 +6,18 @@ import { Link } from 'react-router-dom'
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user)
   const [userPosts, setUserPosts] = useState([])
-
+  const [showMore,setShowMore] = useState(true);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`)
         const data = await res.json()
-        console.log("API response:", data)
+        
         if (res.ok) {
-          setUserPosts(data.posts)
+          setUserPosts(data.posts);
+          if (data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message)
@@ -24,6 +27,22 @@ export default function DashPosts() {
       fetchPosts()
     }
   }, [currentUser._id, currentUser.isAdmin])
+  const handleShowMore = async ()=>{
+    const startIndex=userPosts.length;
+    try{
+      const res=await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIdex}`);
+      const data=await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9){
+          setShowMore(false);
+        }
+      }
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -74,6 +93,14 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {
+            showMore && (
+              <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                Show more
+              </button>
+
+            )
+          }
         </>
     
       ) : (
