44109627734a500b95484a4b079dca20874f29bf
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2025-08-24 11:55:50 +0530

    add recent article section to post page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-import flowbite from 'flowbite/plugin';
-import tailwindScrollbar from 'tailwind-scrollbar';
+// tailwind.config.js
+import flowbite from "flowbite/plugin";
+import tailwindScrollbar from "tailwind-scrollbar";
 
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
