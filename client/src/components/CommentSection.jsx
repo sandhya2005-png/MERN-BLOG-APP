097d81b30df0f6d554bff0a8fe383934adf4d042
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2025-08-24 09:15:44 +0530

    add edit functionality to comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -86,9 +86,17 @@ export default function CommentSection({postId}) {
                     
                 }
         } catch (error) {
-            console.log(message);
+            console.log(error.message);
         }
     }
+    const handleEdit = (commentId, editedContent) => {
+        setComments((prevComments) =>
+            prevComments.map((c) =>
+            c._id === commentId ? { ...c, content: editedContent } : c
+            )
+        );
+    };
+
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
         {currentUser ?
@@ -149,7 +157,7 @@ export default function CommentSection({postId}) {
                     <CommentItem
                         key={comment._id}
                         comment={comment}
-                        onLike={handleLike}
+                        onEdit={handleEdit}
                     />
                 ))
             }
